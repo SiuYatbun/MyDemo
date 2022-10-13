326cccbac6c5c5bd69949c414cabb1341a3d2167
yatbun <281380366@qq.com>
2022-10-13 14:18:11 +0800

    test:修改MyTest，新增Springboot测试类MySpringbootTest

---
+++ src/test/java/com/example/mydemo/MySpringbootTest.java
@@ -0,0 +1,13 @@
+package com.example.mydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MySpringbootTest {
+
+    @Test
+    void test(){
+
+    }
+}
--- src/test/java/com/example/mydemo/MyTest.java
+++ src/test/java/com/example/mydemo/MyTest.java
@@ -1,28 +1,19 @@
 package com.example.mydemo;
 
-import com.example.mydemo.common.mail.MeetingData;
-import com.example.mydemo.common.util.DateUtil;
 import com.example.mydemo.common.util.MailUtil;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.util.Date;
-
-class MyTest {
+public class MyTest {
     @Test
-    void sendMeetingMail() throws IOException {
-        Date startDate = DateUtil.parse("20220505", "yyyyMMdd");
-        Date endDate = DateUtil.parse("20220506", "yyyyMMdd");
+    void send_mail() {
         String to = MailUtil.PROPS.getProperty("receiver");
         String subject = "标题";
         String content = "Hello,world.";
-        MeetingData meetingData = new MeetingData();
-        meetingData.setTo(to);
-        meetingData.setStartTime(DateUtil.format(startDate, "yyyyMMdd'T'HHmmss"));
-        meetingData.setEndTime(DateUtil.format(endDate, "yyyyMMdd'T'HHmmss"));
-        meetingData.setLocation("会议室");
-        MailUtil.sendMeetingMail(to, subject, content, meetingData);
-
+        MailUtil.sendCommonMail(to, subject, content);
     }
 
+    @Test
+    public void test() {
+
+    }
 }
